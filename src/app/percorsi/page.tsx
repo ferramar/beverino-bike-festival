8e6a72a31e923f26dddb4302919341552ac2b260
lollomag <lorenzo.cadente@yahoo.it>
2025-05-26 18:08:28 +0200

    create blank pages for the future

---
+++ src/app/percorsi/page.tsx
@@ -0,0 +1,14 @@
+import { Box, Container } from "@mui/material";
+
+export default function Percorsi() {
+  return (
+    <Box component="main" sx={{
+      pt: "8rem",
+      pb: 4
+    }}>
+      <Container maxWidth="lg">
+        Pagina in costruzione
+      </Container>
+    </Box>
+  );
+}
