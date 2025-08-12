03ae68b9e8e89acbec7197d5236629a95f1f286f
Lorenzo Cadente <lorenzo.cadente@yahoo.it>
2025-08-12 12:41:44 +0200

    update page size sponsor

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -43,7 +43,7 @@ const getFullUrl = (url?: string): string | undefined => {
 };
 
 export async function getAllSponsors(): Promise<SponsorItem[]> {
-  const { data } = await fetcher('/api/sponsors?populate=*&pagination[pageSize]=50');
+  const { data } = await fetcher('/api/sponsors?populate=*&pagination[pageSize]=80');
   if (!data || !Array.isArray(data)) return []
 
   return data.map((item: SponsorStrapi): SponsorItem => {
@@ -60,7 +60,7 @@ export async function getAllSponsors(): Promise<SponsorItem[]> {
 }
 
 export async function getAllSponsorsWithCategories(): Promise<SponsorItem[]> {
-  const { data } = await fetcher('/api/sponsors?populate=*&sort=principale:desc,nome:asc&pagination[pageSize]=50');
+  const { data } = await fetcher('/api/sponsors?populate=*&sort=principale:desc,nome:asc&pagination[pageSize]=80');
   if (!data || !Array.isArray(data)) return []
 
   return data.map((item: SponsorStrapi): SponsorItem => {
